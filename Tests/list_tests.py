7bcd11f6b9c1e401a2c5c6adab7f4e2d35735573
Piotr Dziwinski <piotrdz@gmail.com>
2015-08-23 13:39:00 +0200

    Add option to run tests with given filter

--- Tests/list_tests.py
+++ Tests/list_tests.py
@@ -1,14 +1,25 @@
 #!/usr/bin/env python3
 import unittest
+import os
 
-loader = unittest.TestLoader()
-testmodules = loader.discover('.', pattern='*_test.py')
+def list_tests():
+    tests = []
 
-# .py modules
-for testmodule in testmodules:
-    # TestSuite classes in each module
-    for testsuite in testmodule._tests:
-        # testcase functions in each TestSuite
-        for test in testsuite._tests:
-            testname = test.__str__().split()
-            print(testname[1][1:-1] + "." + testname[0])
+    loader = unittest.TestLoader()
+    this_dir = os.path.dirname(os.path.realpath(__file__))
+    testmodules = loader.discover(this_dir, pattern='*_test.py')
+
+    # .py modules
+    for testmodule in testmodules:
+        # TestSuite classes in each module
+        for testsuite in testmodule._tests:
+            # testcase functions in each TestSuite
+            for test in testsuite._tests:
+                testname = test.__str__().split()
+                tests.append(testname[1][1:-1] + "." + testname[0])
+
+    return tests
+
+if __name__ == '__main__':
+    for test in list_tests():
+        print(test)
--- Tests/run_tests.py
+++ Tests/run_tests.py
@@ -3,14 +3,22 @@ import argparse
 import subprocess
 import os
 import sys
+import list_tests
+import fnmatch
 
 parser = argparse.ArgumentParser(add_help = False)
 parser.add_argument('--colobot-lint-exec', dest='colobot_lint_executable', required=True)
 parser.add_argument('--debug', dest='debug_flag', action='store_true')
+parser.add_argument('--filter', dest='filter')
 options, remaining_args = parser.parse_known_args()
 
 python_interp = sys.executable
 this_dir = os.path.dirname(os.path.realpath(__file__))
+
+if options.filter:
+    for test in list_tests.list_tests():
+        if fnmatch.fnmatch(test, options.filter):
+            remaining_args.append(test)
 
 if len(remaining_args) == 0:
     remaining_args = ['discover', '-s', this_dir, '-t', this_dir, '-p', '*_test.py']
